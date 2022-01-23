c3b46ad424c49ec22c257cc0195055ad4d509a02
Krishna <krishnas@testvagrant.com>
2022-01-23 20:01:02 +0530

    Added Create User Test

--- src/test/java/Sampletest.java
+++ src/test/java/Sampletest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
@@ -15,5 +16,24 @@ public class Sampletest {
                 .then()
                 .statusCode(200)
                 .log().body();
+    }
+
+    @Test
+    public void createUser(){
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer 39856e05ce7d1e8b73a9e56bdd5becb67ff2d4bf89faff36950d1a7ff258663d")
+                .body("{\n" +
+                        "\t\"name\":\"Philip Boston\",\n" +
+                        "\t\"email\": \"philip17@gmail.com\",\n" +
+                        "\t\"gender\": \"male\",\n" +
+                        "\t\"status\": \"active\"\n" +
+                        "}")
+                .when()
+                    .post("https://gorest.co.in/public/v1/users")
+                .then()
+                    .statusCode(201)
+                    .log().body();
     }
 }
