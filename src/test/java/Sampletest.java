82aa8bc4a67602e967ca5ca3f14040ec01b31d6a
Krishna Singhal <krishna@Krishnas-MacBook-Pro.local>
2022-01-23 15:10:05 +0530

    First API test, used Get request

--- src/test/java/Sampletest.java
+++ src/test/java/Sampletest.java
@@ -1,9 +1,19 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
 
 public class Sampletest {
     @Test
-    public void shouldTest(){
-        Assert.assertEquals(1,1);
+    public void shouldGetAllUsers(){
+        //Arrange
+        //Act
+        //Assert
+
+        given()
+                .when()
+                .get("https://gorest.co.in/public/v1/users")
+                .then()
+                .statusCode(200)
+                .log().body();
     }
 }
