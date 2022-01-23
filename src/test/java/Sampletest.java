041040192af809468b2d9e91eab2439e35750887
Krishna <krishnas@testvagrant.com>
2022-01-23 23:14:14 +0530

    Added assertions using matchers

--- src/test/java/Sampletest.java
+++ src/test/java/Sampletest.java
@@ -1,4 +1,6 @@
 import io.restassured.http.ContentType;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
@@ -15,7 +17,9 @@ public class Sampletest {
                 .get("https://gorest.co.in/public/v1/users")
                 .then()
                 .statusCode(200)
-                .log().body();
+                .log().body()
+                .body("data", Matchers.hasSize(20))
+                .body("data", Matchers.hasItem(Matchers.hasEntry("gender", "male")));
     }
 
     @Test
@@ -26,14 +30,16 @@ public class Sampletest {
                 .header("Authorization", "Bearer 39856e05ce7d1e8b73a9e56bdd5becb67ff2d4bf89faff36950d1a7ff258663d")
                 .body("{\n" +
                         "\t\"name\":\"Philip Boston\",\n" +
-                        "\t\"email\": \"philip21@gmail.com\",\n" +
+                        "\t\"email\": \"philip23@gmail.com\",\n" +
                         "\t\"gender\": \"male\",\n" +
                         "\t\"status\": \"active\"\n" +
                         "}")
                 .when()
                     .post("https://gorest.co.in/public/v1/users")
                 .then()
+                    .statusCode(201)
                     .log().body()
-                    .statusCode(201);
+                    .body("data.id", Matchers.notNullValue())
+                    .body("data.email", Matchers.equalTo("philip23@gmail.com"));
     }
 }
